cbbe5184827ef647e2eba12a528187de98ba3991
Helen Woldesenbet <helygwb@iastate.edu>
2020-11-16 17:44:47 -0600

    added reminder feature

---
+++ app/src/main/java/com/example/macros/ui/Reminder/AlarmReceiver.java
@@ -0,0 +1,58 @@
+package com.example.macros.ui.Reminder;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private static final String CHANNEL_ID = "CHANNEL_SAMPLE";
+
+
+    @Override
+    public void onReceive(Context context, Intent inte) {
+
+        // Get id & message
+        int notificationId = inte.getIntExtra("notificationId", 0);
+        String message = inte.getStringExtra("message");
+
+        // Call ReminderFragment when notification is tapped.
+        Intent mainIntent = new Intent(context, ReminderFragment.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
+
+        // NotificationManager
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // For API 26 and above
+            CharSequence channelName = "My Notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, importance);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        // Prepare Notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_lock_idle_alarm)
+                .setContentTitle("Eat more Carbs")
+                .setContentText(mainIntent.getStringExtra("message"))
+                .setContentIntent(contentIntent)
+                .setStyle(new NotificationCompat.InboxStyle()
+                .addLine("Eat healthy")
+                .addLine("Eat more proteins"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(false);
+
+
+        // Notify
+        notificationManager.notify(notificationId, builder.build());
+    }
+}
---
+++ app/src/main/java/com/example/macros/ui/Reminder/ReminderFragment.java
@@ -0,0 +1,112 @@
+package com.example.macros.ui.Reminder;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.macros.R;
+
+
+public class ReminderFragment extends Fragment {
+
+    Button setter;
+    Button cancel;
+    EditText editText;
+    TimePicker timePicker;
+
+    private int notificationId = 1;
+    Intent intent ;
+
+    public ReminderFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_reminder, container, false);
+
+       setter = root.findViewById(R.id.setBtn);
+       cancel = root.findViewById(R.id.cancelBtn);
+       timePicker = root.findViewById(R.id.timePicker);
+       editText = root.findViewById(R.id.editText);
+
+     intent  = new Intent(getActivity(), AlarmReceiver.class);
+        intent.putExtra("notificationId", notificationId);
+        intent.putExtra("message", editText.getText().toString());
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                getActivity(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT
+        );
+
+
+        AlarmManager alarmManager =( AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
+
+
+        setter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int hour = timePicker.getCurrentHour();
+                int minute = timePicker.getCurrentMinute();
+
+                // Create time.
+                Calendar startTime = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    startTime = Calendar.getInstance();
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    startTime.set(Calendar.HOUR_OF_DAY, hour);
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    startTime.set(Calendar.MINUTE, minute);
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    startTime.set(Calendar.SECOND, 0);
+                }
+                long alarmStartTime = 0;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    alarmStartTime = startTime.getTimeInMillis();
+                }
+
+                // Set Alarm
+                alarmManager.set(AlarmManager.RTC_WAKEUP, alarmStartTime, pendingIntent);
+                Toast.makeText(getActivity(), "Done!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alarmManager.cancel(pendingIntent);
+                Toast.makeText(getActivity(), "Canceled.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return root;
+
+    }
+
+
+}
