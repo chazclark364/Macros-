f6f9cc19e30f15717660e1546f71c6f0c696dc5d
Helen Woldesenbet <helygwb@iastate.edu>
2020-11-17 21:07:00 -0600

    added reminder as a top level destination

--- app/src/main/java/com/example/macros/MainActivity.java
+++ app/src/main/java/com/example/macros/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.macros;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_news)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_news,R.id.navigation_reminder)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
